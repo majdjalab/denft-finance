a647d758d1089d5365e841201863a60729adbcf0
majd jalab <majdjalab@gmail.com>
2023-02-09 20:46:00 +0400

    add link to button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Button = ({ styles }) => (
-  <button type="button" className={`py-4 px-6 font-PlayfairDisplay font-medium text-[18px] text-primary z outline-none rounded-[40px] ${styles}`}>
+  <button onClick={(e) => {
+    e.preventDefault();
+    window.location.href='https://github.com/majdjalab/DeNft';
+    }}
+  type="button" className={`py-4 px-6 font-PlayfairDisplay font-medium text-[18px] text-primary z outline-none rounded-[40px] ${styles}`}>
     Source Code
   </button>
 );
--- src/components/SourceCode.jsx
+++ src/components/SourceCode.jsx
@@ -1,7 +1,11 @@
 import styles from "../style";
 
 const SourceCode = () => (
-  <div className={`${styles.flexCenter} w-[200px] h-[70px] rounded-[40px] border-2 border-[#FFBF00] p-[2px] cursor-pointer`}>
+  <div onClick={(e) => {
+    e.preventDefault();
+    window.location.href='https://github.com/majdjalab/DeNft';
+    }}
+  className={`${styles.flexCenter} w-[200px] h-[70px] rounded-[40px] border-2 border-[#FFBF00] p-[2px] cursor-pointer`}>
     <div className={`${styles.flexCenter} flex-col bg-transparent w-[100%] h-[100%] rounded-[40px]`}>
       <div className={`${styles.flexStart} flex-row`}>
         <p className="font-PlayfairDisplay font-medium text-[18px] leading-[23.4px]">
